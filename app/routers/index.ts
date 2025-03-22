3a25f2746240254ac1be23dd4b5b8ce6bddfc57a
Sofiane <ls_yekene@esi.dz>
2025-03-22 15:18:39 +0100

    feat: Implement account management routes and handlers for profile operations

--- app/routers/index.ts
+++ app/routers/index.ts
@@ -5,6 +5,7 @@ import adminProductRouter from './admin/product/product.router';
 
 import commercialDispositiveRouter from './commercial/dispositive/dispositive.router'
 import commercialProductRouter from './commercial/product/product.router'
+import accountRouter from './account/account.router';
 
 
 
@@ -41,6 +42,11 @@ const registerRoutes = (fastify: FastifyInstance) => {
   
   //register sales routes from index.ts in sales folder
   fastify.register(registerSalesRoutes, { prefix: '/sales' });
+
+  // Register account routes
+  fastify.register(accountRouter, { prefix: '/account' });
+
+  
 };
 
 export default registerRoutes;
