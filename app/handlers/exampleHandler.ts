1f2e747ecd1f31671678bea0552eff61012f8088
soufilouai <ll_soufi@esi.dz>
2025-03-22 21:27:53 +0100

    test cicd

--- app/handlers/exampleHandler.ts
+++ app/handlers/exampleHandler.ts
@@ -2,7 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 
 async function getExample(request: FastifyRequest, reply: FastifyReply) {
   // Handler logic
-  return { message: 'Hello from example handler! ci/cd working' };
+  return { message: 'Hello from example handler! ci/cd working ?' };
 }
 
 export { getExample };
