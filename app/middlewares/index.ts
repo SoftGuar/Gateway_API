0b6a9f487e11f13db0e073ecab9b94aa22c0b5aa
Sofiane <ls_yekene@esi.dz>
2025-04-29 23:44:46 +0100

    Enhance CORS middleware configuration to specify allowed methods and headers

--- app/middlewares/index.ts
+++ app/middlewares/index.ts
@@ -4,6 +4,8 @@ import fastifyCors from '@fastify/cors';
 const registerMiddlewares = (fastify: FastifyInstance) => {
   fastify.register(fastifyCors, {
     origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   });
 };
 
